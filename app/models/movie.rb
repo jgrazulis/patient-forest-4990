adae2e472fd6302178ebff6cdf5ad0e7b6860877
Jessica Grazulis <jlgrazulis@gmail.com>
2021-12-14 15:51:23 -0700

    Add studios controller and studio#index route

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ApplicationRecord
   belongs_to :studio
-  has many :movie_actor 
-  has many :actors, through: :movie_actor
+  has_many :movie_actors
+  has_many :actors, through: :movie_actors
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -3,5 +3,14 @@ require 'rails_helper'
 RSpec.describe Movie, type: :model do
   describe 'relationships' do
     it { should belong_to(:studio) }
+    it { should have_many(:movie_actors) }
+    it { should have_many(:actors).through(:movie_actors) }
+  end
+
+  describe 'content' do
+    movie_1 = Movie.create(title: "Raiders of the Lost Ark", creation_year: 1981, genre: "Action/Adventure")
+    expect(page).to have_content(movie_1.title)
+    expect(page).to have_content(movie_1.creation_year)
+    expect(page).to have_content(movie_1.genre)
   end
 end
